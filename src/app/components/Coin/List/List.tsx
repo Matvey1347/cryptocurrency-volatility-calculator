853772f2611232b040fc5a10a65f715a8847730f
Matvey <jedi@red-carlos.co>
2024-08-13 14:06:47 +0200

    Test Vercel

--- src/app/components/Coin/List/List.tsx
+++ src/app/components/Coin/List/List.tsx
@@ -6,6 +6,7 @@ interface ListProps {
 export default function List({ coins }: ListProps) {
   return (
     <ul>
+      <h1>Test</h1>
       {coins.map((coin) => (
         <li key={coin.id} className="p-2 border-b border-gray-300 flex items-center">
           <img src={coin.image} alt={coin.name} className="w-8 h-8 mr-4" />
