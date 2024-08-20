17b92c58f2ae76bf0b7e68729a08cedf492e97a8
Matvey <jedi@red-carlos.co>
2024-08-20 09:40:17 +0200

    Remove price from list

--- src/app/coin/components/List/List.tsx
+++ src/app/coin/components/List/List.tsx
@@ -13,7 +13,7 @@ export default function List({ coins }: ListProps) {
         <li key={coin.id} className="p-2 border-b border-gray-300 flex items-center hover:bg-gray-100">
           <Link href={`/coin/${coin.id}`} className="flex items-center w-full">
             <img src={coin.image || coin.large} alt={coin.name} className="w-8 h-8 mr-4" />
-            <span>{coin.name} ({coin.symbol}) {(coin.current_price) ? ' - ' + coin.current_price : ''}</span>
+            <span>{coin.name} ({coin.symbol})</span>
           </Link>
         </li>
       ))}
